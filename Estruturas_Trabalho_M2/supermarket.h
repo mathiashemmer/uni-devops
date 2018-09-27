16783da9fae33bdd9f14af873cf62e9c2e5e0abf
kaiofreis <kaiofreis@hotmail.com>
2018-09-27 13:30:27 -0300

    supermaRRRRket

---
+++ Estruturas_Trabalho_M2/supermarket.h
@@ -0,0 +1,13 @@
+#ifndef SUPERMARKET_H
+#define SUPERMARKET_H
+
+include <iostream>
+
+struct Customer
+{
+    int timeStamp;
+    int timeElapsed;
+    std::string name;
+}
+
+#endif // SUPERMARKET_H
