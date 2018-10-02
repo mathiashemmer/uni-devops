426c6e65dd21eeed8504bf2b8a4085839b078191
mathiashemmer <mathiasubatuba@gmail.com>
2018-10-02 13:05:51 -0300

    ajustasdasdas

--- POO_Trabalho_M2/mainwindow.cpp
+++ POO_Trabalho_M2/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,26 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    if(this->lista1.size() > 0){
+        int a = this->lista1.last();
+        this->lista1.append(a+1);
+    }else{
+        this->lista1.append(1);
+    }
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QMessageBox mybox;
+    if (QMessageBox::Yes == QMessageBox::question(this,
+                                                  tr("title"),
+                                                  tr(QString::number(this->lista1.last()).toStdString().c_str())))
+    {
+        // do stuff
+    }
 }
--- POO_Trabalho_M2/mainwindow.h
+++ POO_Trabalho_M2/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QList>
 
 namespace Ui {
 class MainWindow;
@@ -12,8 +13,15 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    QList<int> lista1;
+    QList<double> lista2;
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
 
 private:
     Ui::MainWindow *ui;
